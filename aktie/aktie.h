9700ce48ad4e5128a541cf764ad50c33a31a892b
AytiKara <aytac.karakaya@hotmail.com>
2016-04-07 16:51:01 +0200

    Erstellen
    s

---
+++ aktie/aktie.h
@@ -0,0 +1,9 @@
+#ifndef AKTIE_H_INCLUDED
+#define AKTIE_H_INCLUDED
+
+class Aktie
+{
+public:
+    Aktie(Aktie n);
+};
+#endif // AKTIE_H_INCLUDED
---
+++ aktie/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+int main()
+{
+    ifstream f;  // Datei-Handle
+    string s;int lenge=1;
+    f.open("msft.csv", ios::in); // Öffne Datei aus Parameter
+    while (!f.eof())          // Solange noch Daten vorliegen
+    {
+        getline(f, s);        // Lese eine Zeile
+        cout << s << endl;    // Zeige sie auf dem Bildschirm
+        lenge++;if(lenge>30)break;
+
+    }
+    f.close();
+
+    return 0;
+}
